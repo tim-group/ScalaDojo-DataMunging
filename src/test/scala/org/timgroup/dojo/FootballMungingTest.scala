b507a6437834cb958f03803367de4363be89c687
Pawel Krupinski <pawel.krupinski@youdevise.com>
2012-05-29 16:37:37 +0100

    Minor formatting changes.

--- src/test/scala/org/timgroup/dojo/FootballMungingTest.scala
+++ src/test/scala/org/timgroup/dojo/FootballMungingTest.scala
@@ -5,7 +5,6 @@ import org.scalatest.matchers.ShouldMatchers
 
 class FootballMungingTest extends FunSuite with ShouldMatchers with ReadFile {
 
-
   test("The team with smallest difference between for and against is Aston Villa") {
     val teamWithTheSmallestDifferenceBetweenForAndAgainst = FootballMunging(readFile("football.dat"))
       .teamWithTheSmallestDifferenceBetweenForAndAgainst
--- src/test/scala/org/timgroup/dojo/WeatherMungingTest.scala
+++ src/test/scala/org/timgroup/dojo/WeatherMungingTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 
 class WeatherMungingTest extends FunSuite with ShouldMatchers with ReadFile {
+
   test("Determines the team with the smallest spread between for and agains") {
     val dayWithSmallestTemperatureSpread = WeatherMunging(readFile("weather.dat")).dayWithTheSmallestTemperatureSpread
 
